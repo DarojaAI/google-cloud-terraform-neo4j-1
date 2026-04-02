2ec5a701a169c984733316413954b9a3c23f950e
Ben Lackey <ben.lackey@outlook.com>
2026-04-02 13:04:33 -0700

    move to mp image

--- ee/variables.tf
+++ ee/variables.tf
@@ -16,8 +16,7 @@ variable "goog_cm_deployment_name" {
 variable "source_image" {
   description = "Base image for the VM instance."
   type        = string
-  #default     = "projects/neo4j-mp-public/global/images/neo4j-enterprise-edition-v20260126"
-  default     = "projects/centos-cloud/global/images/centos-stream-10-v20260126"
+  default     = "projects/neo4j-mp-public/global/images/neo4j-enterprise-edition-v20260126"
 }
 
 ##########################################
